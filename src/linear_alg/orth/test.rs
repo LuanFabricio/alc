8f96b4715216802b81ff79e402f15e6ff4087a29
LuanFabricio <luanlimaleite@hotmail.com>
2023-07-30 19:18:08 -0300

    implementando testes para a decomposição QR

--- src/linear_alg/orth/test.rs
+++ src/linear_alg/orth/test.rs
@@ -27,8 +27,6 @@ mod norm {
             }
         }
 
-        println!("{:?}", a.content);
-
         assert!(a.norm() > 0_f32);
     }
 }
@@ -154,6 +152,109 @@ mod ortho_gram_schmidt {
         for i in 0..ROWS {
             for j in 0..COLUMNS {
                 assert!(res[i][j] - indentity[i][j] < f32::EPSILON);
+            }
+        }
+    }
+}
+
+mod dec_rq {
+    use super::*;
+
+    #[test]
+    fn should_the_q_and_r_results_should_generate_the_original_matrix() {
+        const ROWS: usize = 5;
+        const COLUMNS: usize = 4;
+
+        let mut matrix = Matrix::new(ROWS, COLUMNS, None);
+
+        // Writing the original matrix.
+        for i in 0..ROWS {
+            for j in 0..COLUMNS {
+                matrix[i][j] = 1_f32 + (i * COLUMNS + j) as f32;
+            }
+        }
+
+        let (q, r) = matrix.dec_qr();
+
+        let qr = q * r;
+
+        for i in 0..ROWS {
+            for j in 0..COLUMNS {
+                assert!(matrix[i][j] - qr[i][j].round() < f32::EPSILON);
+            }
+        }
+    }
+
+    #[should_panic]
+    #[test]
+    fn should_panic_if_matrix_rows_are_bigger_than_columns() {
+        const ROWS: usize = 3;
+        const COLUMNS: usize = 4;
+
+        let matrix = Matrix::new(ROWS, COLUMNS, None);
+
+        matrix.dec_qr();
+    }
+
+    #[test]
+    fn should_calc_qr() {
+        const ROWS: usize = 4;
+        const COLUMNS: usize = 3;
+
+        let mut matrix = Matrix::new(ROWS, COLUMNS, None);
+        matrix[0] = vec![1_f32, 1_f32, 1_f32];
+        matrix[1] = vec![0_f32, 1_f32, 1_f32];
+        matrix[2] = vec![1_f32, 1_f32, 1_f32];
+        matrix[3] = vec![1_f32, 0_f32, 1_f32];
+
+        let mut expected_q = Matrix::new(ROWS, COLUMNS, None);
+        expected_q[0] = vec![
+            1_f32 / 3_f32.sqrt(),
+            1_f32 / 15_f32.sqrt(),
+            -(10_f32.sqrt()) / 10_f32,
+        ];
+
+        expected_q[1] = vec![0_f32, 3_f32 / 15_f32.sqrt(), 10_f32.sqrt() / 10_f32];
+
+        expected_q[2] = vec![
+            1_f32 / 3_f32.sqrt(),
+            1_f32 / 15_f32.sqrt(),
+            -(10_f32.sqrt()) / 10_f32,
+        ];
+
+        expected_q[3] = vec![
+            1_f32 / 3_f32.sqrt(),
+            -2_f32 / 15_f32.sqrt(),
+            10_f32.sqrt() / 10_f32,
+        ];
+
+        let mut expected_r = Matrix::new(COLUMNS, COLUMNS, None);
+
+        expected_r[0] = vec![3_f32.sqrt(), 2_f32 / 3_f32.sqrt(), 3_f32.sqrt()];
+
+        expected_r[1] = vec![0_f32, 15_f32.sqrt() / 3_f32, 3_f32 / 15_f32.sqrt()];
+
+        expected_r[2][2] = 10_f32.sqrt() / 5_f32;
+
+        let (q, r) = matrix.dec_qr();
+
+        assert_eq!(q.rows, expected_q.rows);
+        assert_eq!(q.columns, expected_q.columns);
+
+        assert_eq!(r.rows, expected_r.rows);
+        assert_eq!(r.columns, expected_r.columns);
+
+        // Testing q
+        for i in 0..ROWS {
+            for j in 0..COLUMNS {
+                assert!(expected_q[i][j] - q[i][j] < f32::EPSILON);
+            }
+        }
+
+        // Testing r
+        for i in 0..COLUMNS {
+            for j in 0..COLUMNS {
+                assert!(expected_r[i][j] - r[i][j] < f32::EPSILON);
             }
         }
     }
